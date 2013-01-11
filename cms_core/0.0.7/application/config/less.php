383087a1a1b70144b85e69fe6a35f04a97e38d11
Paulo Carvalho <paulocarvalhofrediani@gmail.com>
2013-01-11 11:43:32 +0000

    less config

--- cms_core/0.0.7/application/config/less.php
+++ cms_core/0.0.7/application/config/less.php
@@ -1,11 +1,15 @@
 <?php
 
+$theme 	= CMS::set_theme();
+
 return array(
             'directories' => array(
-            
+            	 $_SERVER['DOCUMENT_ROOT'] . '/public/themes/'.$theme->name.'/less' => $_SERVER['DOCUMENT_ROOT'] . '/public/themes/'.$theme->name.'/css',
             ),
+            
             'files' => array(
-              
+               
+                //path('public') . 'themes/default/less/theme.less' => path('public') . 'themes/default/css/theme.css',
             ),
             'snippets' => array(
               
