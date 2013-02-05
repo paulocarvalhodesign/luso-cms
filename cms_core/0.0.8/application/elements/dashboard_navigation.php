978d0182e390f62f069997ea795efc9e6a0062af
Paulo Carvalho <paulocarvalhofrediani@gmail.com>
2013-02-05 13:47:59 +0000

    Upgrading to 0.0.8
    Signed-off-by: Paulo Carvalho <paulocarvalhofrediani@gmail.com>

--- cms_core/0.0.8/application/elements/dashboard_navigation.php
+++ cms_core/0.0.8/application/elements/dashboard_navigation.php
@@ -8,7 +8,7 @@
      <li><a href=" <?php echo url('settings') ;?> "><i class="icon-wrench"></i> Settings</a> </li>
      <li><a href=" <?php echo url('extensions') ;?> "><i class="icon-th"></i> Extensions & Blocks</a> </li>
   
-     <a class="frontend_btn tt" rel="tooltip" data-placement="top" data-original-title="Frontend " href="<?php echo url('/');?>"><i class="icon-globe icon-white"></i></a>
+     <a class="frontend_btn tt" rel="tooltip" data-placement="top" data-original-title="Frontend " href="<?php echo url('/');?>"><i class="icon-globe icon"></i></a>
 </ul>
 <script>
 $(document).ready(function() {
